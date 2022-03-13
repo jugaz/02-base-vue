11c66300cac7c01e4ca7028f49a14f3a6f1a98e6
Jesús Antonio Ugaz Vegas <55292033+jugaz@users.noreply.github.com>
2022-03-13 11:32:15 -0500

    Add files via upload

---
+++ bases/app.js
@@ -0,0 +1,27 @@
+﻿
+const app = Vue.createApp({
+    // template: `
+    //     <h1> hola Mundo </h1>
+    //     <p> Desde app.js </p>
+    // `
+    data() {
+        return {
+            quote: "I'm Batama",
+            author: "Bruce Wayne",
+        }
+    },
+    methods: {
+        changeQuote() {
+            console.log( 'Hola mundo' )
+            this.author = "Jesús Ugaz"
+            this.capitalize()
+           
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
+        }
+            
+    }
+})
+
+app.mount('#myApp')
